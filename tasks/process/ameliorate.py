49e61fb992b652da88c1a04ed6e629acd83d59ca
pitlst <1442450941@qq.com>
2025-03-22 21:34:04 +0800

    改善提交下一版

--- tasks/process/ameliorate.py
+++ tasks/process/ameliorate.py
@@ -9,16 +9,35 @@ class ameliorate(process):
     
     def task_main(self) -> None:
         data_index = pd.read_csv(os.path.join(CONFIG.TABLE_PATH, "城轨事业部改善指标.csv"), encoding="utf-8")
-        data: pd.DataFrame = self.connect.sql(
+        # data: pd.DataFrame = self.connect.sql(
+        #     f"""
+        #         SELECT 
+        #             bill."组室",
+        #             COUNT(bill."id") AS "提交数量"
+        #         FROM ods.ameliorate AS bill
+        #         WHERE bill."提案单位一级" = '城轨事业部' 
+        #         AND bill."提交日期" >= DATE_TRUNC('MONTH', CURRENT_DATE)
+        #         AND bill."提交日期" < DATE_TRUNC('MONTH', CURRENT_DATE) + INTERVAL '1 MONTH'
+        #         GROUP BY bill."组室"
+        #     """
+        # ).fetchdf()
+        data = self.connect.sql(
             f"""
-                SELECT 
-                    bill."组室",
-                    COUNT(bill."id") AS "提交数量"
-                FROM ods.ameliorate AS bill
-                WHERE bill."提案单位一级" = '城轨事业部' 
-                AND bill."提交日期" >= DATE_TRUNC('MONTH', CURRENT_DATE)
-                AND bill."提交日期" < DATE_TRUNC('MONTH', CURRENT_DATE) + INTERVAL '1 MONTH'
-                GROUP BY bill."组室"
+                SELECT
+                    di."组室",
+                    di."指标数",
+                    COALESCE(bill."提交数量", 0) AS "提交数量"
+                FROM data_index di
+                LEFT JOIN (
+                    SELECT 
+                        bill."组室",
+                        COUNT(bill."id") AS "提交数量"
+                    FROM ods.ameliorate AS bill
+                    WHERE bill."提案单位一级" = '城轨事业部' 
+                    AND bill."提交日期" >= DATE_TRUNC('MONTH', CURRENT_DATE)
+                    AND bill."提交日期" < DATE_TRUNC('MONTH', CURRENT_DATE) + INTERVAL '1 MONTH'
+                    GROUP BY bill."组室"
+                ) da ON di."组室" = da."组室"
             """
         ).fetchdf()
         print(data)
