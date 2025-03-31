2912f11bf1c437b514fe129731d526cc63c9bdd7
cheakf <sunmoming#gmail.com>
2025-03-31 09:57:42 +0800

    将循环启动从windows的任务计划程序转移到python程序中

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import time
 from utils.connect import make_coonect
 from tasks.scheduler import scheduler
 from tasks.init import task_init
-        
+
 if __name__ == "__main__":
-    connect_data = make_coonect()
-    _scheduler = scheduler(connect_data.duckdb)
-    tasks = task_init(connect_data)
-    _scheduler.run(tasks)
+    while True:
+        try:
+            connect_data = make_coonect()
+            scheduler(connect_data.duckdb).run(task_init(connect_data))
+        except Exception as e:
+            print(f"主程序出错: {e}")
+        print("运行完成，等待10分钟重新启动ETL负载")
+        time.sleep(600)
