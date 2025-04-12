ae60c633da3cf4f3b857289b49105645a4d7003b
pitlst <1442450941@qq.com>
2025-04-12 12:23:43 +0800

    fix bug

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         try:
             connect_data = make_coonect()
             runner = executer(connect_data.duckdb)
-            
+            runner.run(task_init(connect_data))
         except Exception as e:
             print(f"主程序出错: {e}")
         print("运行完成，等待10分钟重新启动ETL负载")
